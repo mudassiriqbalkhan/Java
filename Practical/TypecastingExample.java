363df158b1c371e2e7f185e495b66593621770a3
mudassiriqbalkhan <123162716+mudassiriqbalkhan@users.noreply.github.com>
2023-01-24 17:25:51 +0530

    Add files via upload

---
+++ Practical/TypecastingExample.java
@@ -0,0 +1,10 @@
+Class TypecastingExample{
+public static void main(String args[]){
+int x = 890;
+long z = x;//auto
+System.out.println(x);
+System.out.println(z);
+double y = x;
+System.out.println(y);
+}
+}

edbae0cac46093fc01fca31a3c6447fdfc78cade
mudassiriqbalkhan <123162716+mudassiriqbalkhan@users.noreply.github.com>
2023-01-24 17:57:05 +0530

    Add files via upload

---
+++ Assignment/ComparisonExample.java
@@ -0,0 +1,15 @@
+class ComparisonExample{
+public void cal(){
+int a=14 , b=16;
+System.out.println("a>b:" +(a>b));
+System.out.println("a<b:" +(a<b));
+System.out.println("a>=b:" +(a>=b));
+System.out.println("a<=b:" +(a<=b));
+System.out.println("a==b:" +(a==b));
+System.out.println("a!=b:" +(a!=b));
+}
+public static void main(String args[]){
+ComparisonExample obj=new ComparisonExample();
+obj.cal();
+}
+}

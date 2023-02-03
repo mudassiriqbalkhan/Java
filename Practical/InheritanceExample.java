c2caed6f9179458eb5ced234ddc5b70186f11262
mudassiriqbalkhan <123162716+mudassiriqbalkhan@users.noreply.github.com>
2023-02-03 16:21:50 +0530

    Add files via upload

---
+++ Practical/InheritanceExample.java
@@ -0,0 +1,13 @@
+class Owner{
+int area;
+String dateOfPurchase;
+String name;
+}
+class New_Owner{
+
+String dateofregister;
+double flatvalue;
+}
+Class InheritanceExample{
+public static void main(String args[]){
+}

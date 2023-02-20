3886d2393435b6c2907e13ac4c972f0cf76d1a5b
mudassiriqbalkhan <123162716+mudassiriqbalkhan@users.noreply.github.com>
2023-02-20 16:37:33 +0530

    Add files via upload

---
+++ Practical/MultiArrayEG .java
@@ -0,0 +1,14 @@
+public class MultiArrayEG {
+
+	public static void main(String[] args) {
+		int arr1[][]= {{9,8,7},{4,2,8},{4,5,2}};
+ for(int i=0;i<3;i++) {
+	 for(int j=0;j<3;j++) {
+		System.out.print(arr1[i][j]+"");
+	 }
+	 System.out.println();
+ }
+
+	}
+
+}
